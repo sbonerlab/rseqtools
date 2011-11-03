2d94c3ad67e356703a34634534de81ee7c2f55fc
Andrea Sboner <ans2077@med.cornell.edu>
2011-11-03 18:39:34 +0000

    Update: 1. it includes the structure of the exons by default 2. with the optional parameter 'simple' one can obtain only the single subintervals as previously
    git-svn-id: svn+ssh://svn.gersteinlab.org/repos/bioinfo/rseqtools@635 2fd4de08-f6cc-4d2b-b124-baeea9ba92f3

--- src/interval2bed.c
+++ src/interval2bed.c
@@ -17,9 +17,14 @@ int main (int argc, char *argv[])
   Array intervals;
   Interval *currInterval;
   SubInterval *currSubInterval;
+  Stringa sizes =NULL;
+  Stringa starts=NULL;
 
-  if (argc != 2) {
-    usage ("%s <trackName>",argv[0]);
+  if (argc < 2) {
+    usage ("%s <trackName> [simple]",argv[0]);
+  }
+  if( (argc==3) && !strEqual( argv[2],"simple") ) {
+    usage("%s <trackName> [simple]",argv[0]);
   }
   intervalFind_addIntervalsToSearchSpace ("-",0);
   intervals = intervalFind_getAllIntervals ();
@@ -27,10 +32,26 @@ int main (int argc, char *argv[])
   printf ("track name=\"%s\" visibility=2\n",argv[1]);
   for (i = 0; i < arrayMax (intervals); i++) {
     currInterval = arrp (intervals,i,Interval);
-    for (j = 0; j < arrayMax (currInterval->subIntervals); j++) {
-      currSubInterval = arrp (currInterval->subIntervals,j,SubInterval);
-       printf ("%s\t%d\t%d\t%s\t900\t%c\n",
-               currInterval->chromosome,currSubInterval->start,currSubInterval->end,currInterval->name,currInterval->strand);
+    if( argc == 3 ) {
+      for (j = 0; j < arrayMax (currInterval->subIntervals); j++) {
+	currSubInterval = arrp (currInterval->subIntervals,j,SubInterval);
+	printf ("%s\t%d\t%d\t%s\t900\t%c\n",
+		currInterval->chromosome,currSubInterval->start,currSubInterval->end,currInterval->name,currInterval->strand);
+      }
+    } else {
+      stringCreateClear( starts, 10);
+      stringCreateClear( sizes,   10);
+      for( j = 0; j < arrayMax (currInterval->subIntervals); j++) {
+	currSubInterval = arrp (currInterval->subIntervals,j,SubInterval);
+	stringAppendf( sizes, "%d", currSubInterval->end - currSubInterval->start );
+        stringAppendf( starts, "%d", currSubInterval->start - currInterval->start );
+	if( j<arrayMax( currInterval->subIntervals) ) {
+	  stringAppendf( sizes, "," );
+	  stringAppendf(starts, "," );
+	}
+      }
+      printf ("%s\t%d\t%d\t%s\t900\t%c\t%d\t%d\t255,0,0\t%d\t%s\t%s\n",
+	      currInterval->chromosome,currInterval->start,currInterval->end, currInterval->name,currInterval->strand, currInterval->start, currInterval->end, currInterval->subIntervalCount, string(sizes), string(starts) );
     }
   }
   return 0;
