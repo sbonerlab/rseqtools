a2c5c5bd415bd7b0dc5e9692d8f1010297940109
Andrea Sboner <ans2077@med.cornell.edu>
2011-11-09 00:42:51 +0000

    Update: added support for BED12 files
    git-svn-id: svn+ssh://svn.gersteinlab.org/repos/bioinfo/rseqtools@647 2fd4de08-f6cc-4d2b-b124-baeea9ba92f3

--- src/bed2interval.c
+++ src/bed2interval.c
@@ -1,6 +1,8 @@
 #include <bios/format.h>
 #include <bios/log.h>
-#include <mrf/mrfUtil.h>
+#include <bios/intervalFind.h>
+#include <bios/bedParser.h>
+#include <bios/common.h>
 
 
 
@@ -14,15 +16,44 @@
 
 int main (int argc, char *argv[]) 
 {
-  Array tars;
-  Tar *currTar;
-  int i;
+  Array beds;
+  Bed *currBed;
+  int i,j;
 
-  tars = readTarsFromBedFile ("-");
-  for (i = 0; i < arrayMax (tars); i++) {
-    currTar = arrp (tars,i,Tar);
-    printf ("BED_%d\t%s\t.\t%d\t%d\t1\t%d\t%d\n",
-            i + 1,currTar->targetName,currTar->start,currTar->end,currTar->start,currTar->end);
+  bedParser_initFromFile ("-");
+  j=1;
+  while( currBed = bedParser_nextEntry() ) {
+    Interval* currInterval;
+    AllocVar( currInterval );
+    currInterval->chromosome = hlr_strdup( currBed->chromosome );
+    currInterval->start = currBed->start;
+    currInterval->end = currBed->end;
+    if( currBed->extended ) {
+      currInterval->name = hlr_strdup( currBed->name );
+      currInterval->strand = currBed->strand;
+      currInterval->subIntervalCount = currBed->blockCount;
+      currInterval->subIntervals = arrayCreate( currBed->blockCount, SubInterval );
+      for( i=0; i<currBed->blockCount; i++) { 
+	SubInterval *currSubInterval = arrayp( currInterval->subIntervals, arrayMax( currInterval->subIntervals), SubInterval);
+	SubBlock* currSubBlock = arrp( currBed->subBlocks, i, SubBlock );
+	currSubInterval->start = currSubBlock->start + currBed->start;
+	currSubInterval->end = currSubInterval->start + currSubBlock->size;
+      }
+    } else {
+      Stringa tmp = stringCreate( 10 );
+      stringPrintf( tmp, "BED_%d", j);
+      currInterval->name = hlr_strdup( string( tmp )  );
+      stringDestroy( tmp );
+      currInterval->strand = '.';
+      currInterval->subIntervalCount = 1;
+      currInterval->subIntervals = arrayCreate( 1, SubInterval );
+      SubInterval *currSubInterval = arrayp( currInterval->subIntervals, arrayMax( currInterval->subIntervals), SubInterval);
+      currSubInterval->start = currBed->start;
+      currSubInterval->end = currBed->end;
+    }
+    printf( "%s\n", intervalFind_writeInterval( currInterval ) );
+    freeMem( currInterval );
+    j++;
   }
   return 0;
 }
