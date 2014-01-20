a21cf64eb51d1f7ba33609e29a7ba8c5afea7a67
Andrea Sboner <andrea.sboner.w@gmail.com>
2014-01-20 15:28:49 -0500

    UPDATE: dealing with Cigar
    This version tackles the issue of Cigar including soft-clipped bases
    and indels.
    This version correctly identifies reads on the positive strand that are:
    mM - genomic reads, all matches
    m1MnNm2M - splice junction reads
    sSm1MnNm2M - splice junction reads with soft clipped data at the
    beginning of the query

--- src/sam2mrf.c
+++ src/sam2mrf.c
@@ -33,7 +33,7 @@ static char sam_strand_to_mrf_strand(SamEntry* sam_entry, char sam_strand) {
   return mrf_strand;
 }
 
-static void print_mrf_align_blocks(SamEntry *sam_entry, int sam_strand) {
+static void print_mrf_alignment_blocks(SamEntry *sam_entry, int sam_strand) {
   // Parse CIGAR string.
   Array cigar_operations = sam_entry->cigar_ops;
 
@@ -41,28 +41,35 @@ static void print_mrf_align_blocks(SamEntry *sam_entry, int sam_strand) {
   char mrf_strand = sam_strand_to_mrf_strand(sam_entry, sam_strand);
   int target_start = sam_entry->pos;
   int query_start = 1;
+  int query_end = query_start;
   bool first = true;
   for (int i = 0; i < arrayMax(cigar_operations); ++i) {
     CigarOperation* op = arrp(cigar_operations, i, CigarOperation);
     switch (op->type) {
       case kCigarAlignmentMatch: {
         int target_end = target_start + op->length - 1;
-        int query_end = query_start + op->length - 1;
-        printf("%s%s:%c:%d:%d:%d:%d",
-               (first == false) ? "," : "",
-               sam_entry->rname, mrf_strand, target_start, target_end,
-               query_start, query_end);
-        target_start += op->length - 1;
-        query_start += op->length;
+        query_end = query_start + op->length - 1;
+	printf("%s%s:%c:%d:%d:%d:%d",
+	       (first == false) ? "," : "",
+	       sam_entry->rname, mrf_strand, target_start, target_end,
+	       query_start, query_end);
+	first = false;
+        target_start += op->length;
+        query_start += op->length ;
         break;
       }
       case kCigarSkippedRegion: {
-        target_start += op->length = 1;
+        target_start += op->length - 1;
         break;
       }
+      case kCigarSoftClipping: {
+	if ( i == 0 )  query_start += op->length; else query_end = query_start ;
+	break;
+      }
     }
-    first = false;
+    
   }
+  
 }
 
 void print_headers(bool has_seqs, bool has_qual) {
@@ -76,12 +83,14 @@ void print_headers(bool has_seqs, bool has_qual) {
   printf("\t%s\n", MRF_COLUMN_NAME_QUERY_ID);
 }
 
-int sam_to_mrf(char* delim) {
+int sam_to_mrf(char delim) {
   SamParser* parser = samparser_from_file("-");
   Array comments = samparser_get_comments(parser);
-  for (int i = 0; i < arrayMax(comments); ++i) {
-    char* comment = arru(comments, i, char*);
-    printf("# %s\n", comment);
+  if( comments != NULL ) {
+    for (int i = 0; i < arrayMax(comments); ++i) {
+      char* comment = arru(comments, i, char*);
+      printf("# %s\n", comment);
+    }
   }
 
   bool first = true;
@@ -91,7 +100,7 @@ int sam_to_mrf(char* delim) {
     SamEntry* mate_entry = NULL;
     // If entry is not valid, skip it. If it is the first of a pair, then make
     // sure that the mate entry is skipped as well.
-    if (!samentry_is_vaid(entry)) {
+    if (!samentry_is_valid(entry)) { // valid = mapped and primary alignment
       if (!samentry_is_paired(entry)) {
         samentry_free(entry);
         continue;
@@ -102,22 +111,22 @@ int sam_to_mrf(char* delim) {
         break;
       }
       if (!samentry_same_name(entry, mate_entry, delim)) {
-        samparser_back(1);
+        samparser_back(parser, 1);
       }
       samentry_free(entry);
       samentry_free(mate_entry);
       continue;
     }
-
+    
     // If entry is the first of a paired-end, then attempt to read then next 
     // entry and validate that it is the mate of the entry.
     if (samentry_is_paired(entry)) {
       mate_entry = samparser_next_entry(parser);
       if (mate_entry == NULL || !samentry_is_valid(mate_entry)) {
-        samentry_free(entry);
-        if (mate_entry != NULL) {
-          samentry_free(mate_entry);
-        }
+	samentry_free(entry);
+	if (mate_entry != NULL) {
+	    samentry_free(mate_entry);
+	}
         continue;
       }
       if (!samentry_same_name(entry, mate_entry, delim)) {
@@ -126,15 +135,15 @@ int sam_to_mrf(char* delim) {
             "file first.\nEx: sort -r file.sam | sam2mrf > file.mrf\n");
       }
     }
-
+    
     // If this is the first entry, print the MRF headers. Note that we must
     // check whether the entries have the optional sequence and quality score
-    // fields in order to print the correct headers.
-    if (start == true) {
-      has_seqs = samentry_has_seqs(entry);
-      has_qual = samentry_has_qual(entry);
+    // fields in order to print the correct headers. Assuming that all SAM files have sequence and quals
+    if (first == true) {
+      has_seqs = true; //samentry_has_seqs(entry);
+      has_qual = true; //samentry_has_qual(entry);
       print_headers(has_seqs, has_qual);
-      start = false;
+      first = false;
     }
 
     // Print MRF AlignmentBlocks.
@@ -147,17 +156,17 @@ int sam_to_mrf(char* delim) {
     // Print sequence(s) if available.
     if (has_seqs == true) {
       seq_init();
-      if (!samentry_has_seqs(entry)) {
+      /*if (!samentry_has_seqs(entry)) { // assuming it's present
         die("Entry missing sequence column\n");
-      }
+	}*/
       if (entry->flags & S_QUERY_STRAND) {
         seq_reverseComplement(entry->seq, strlen(entry->seq));
       }
       printf("\t%s", entry->seq);
       if (mate_entry != NULL) {
-        if (!samentry_has_seqs(mate_entry)) {
+        /*if (!samentry_has_seqs(mate_entry)) {
           die("Mate entry missing sequence column\n");
-        }
+	  }*/
         if (mate_entry->flags & S_MATE_STRAND) {
           seq_reverseComplement(mate_entry->seq, strlen(mate_entry->seq));
         }
@@ -167,18 +176,18 @@ int sam_to_mrf(char* delim) {
 
     // Print quality score(s) if available.
     if (has_qual == true) {
-      if (!samentry_has_qual(entry)) {
+      /*if (!samentry_has_qual(entry)) {
         die("Entry missing quality scores column");
-      }
+	}*/
       printf("\t%s", entry->qual);
       if (mate_entry != NULL) {
-        if (!samentry_has_qual(mate_entry)) {
+        /*if (!samentry_has_qual(mate_entry)) {
           die("Mate entry missing quality scores column");
-        }
+	  }*/
         printf("|%s", mate_entry->qual);
       }
     }
-
+    
     // Print queryID.
     if (mate_entry == NULL) {
       printf("\t%s", entry->qname);
@@ -188,6 +197,7 @@ int sam_to_mrf(char* delim) {
       printf("\t%s|%s", entry->qname, "2");
     }
 
+    printf("\n");
     samentry_free(entry);
     if (mate_entry != NULL) {
       samentry_free(mate_entry);
@@ -195,13 +205,13 @@ int sam_to_mrf(char* delim) {
   }
   samparser_free(parser);
   return EXIT_SUCCESS;
-}
+  }
 
 int main(int argc, char **argv) {
   char delim = '\0';
   if (argc == 2) {
     delim = argv[1][0];
-  }
+  } 
   return sam_to_mrf(delim);
 }
 
